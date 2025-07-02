e527d4c7fe237748d4a9951efe9eab9782fef86e
Mark Daniel Callejas <callejasmark63@gmail.com>
2025-07-03 02:12:03 +0800

    Redux:L-Dispatching from Forms Solution

--- Redux/React-Redux/src/SetCounter.jsx
+++ Redux/React-Redux/src/SetCounter.jsx
@@ -1,16 +1,31 @@
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { set } from "./action";
+
 export const SetCounter = ({ onSubmit }) => {
-    
+  const countFromStore = useSelector(state => state.count);
+  const [count, setCount] = useState(countFromStore) ;
+  const dispatch = useDispatch();
+
+  useEffect(()=>{
+    setCount(countFromStore)
+  }, [countFromStore]);
+
   return (
     <section>
       <form
         onSubmit={(event) => {
           event.preventDefault();
-          const value = event.target.elements['set-to'].value
-          onSubmit(parseInt(value))
+          dispatch(set(count))
         }}
       >
         <label htmlFor="set-to">Set Count</label>
-        <input id="set-to" type="number" />
+        <input 
+            id="set-to" 
+            type="number"
+            value = {count}
+            onChange={(e) => setCount(e.target.value)}
+         />
         <input type="submit" />
       </form>
     </section>
