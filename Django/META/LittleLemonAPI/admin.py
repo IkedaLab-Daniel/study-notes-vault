ff8b6fde479ec1429ebe4cc83faac8e3f5d15781
Mark Daniel Callejas <callejasmark63@gmail.com>
2025-07-14 01:44:24 +0800

    Revert back to LittleLemonAPI setup / Remove Category samples

--- Django/META/LittleLemonAPI/admin.py
+++ Django/META/LittleLemonAPI/admin.py
@@ -1,6 +1,4 @@
 from django.contrib import admin
 from .models import MenuItem
-from .models import Category
 
 admin.site.register(MenuItem)
-admin.site.register(Category)
--- Django/META/LittleLemonAPI/serializers.py
+++ Django/META/LittleLemonAPI/serializers.py
@@ -1,22 +1,11 @@
 from rest_framework import serializers
 from .models import MenuItem
-from .models import Category
-from decimal import Decimal
-
-class CategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Category
-        fields = ['id', 'slug', 'title']
 
 class MenuItemSerializer(serializers.ModelSerializer):
-    stock = serializers.IntegerField(source='inventory')
-    price_after_tax = serializers.SerializerMethodField(method_name='calculate_tax')
-    # category = CategorySerializer()
-    category_id = serializers.IntegerField(write_only = True)
     class Meta:
         model = MenuItem
-        fields = ['id', 'title', 'price', 'stock', 'price_after_tax', 'category', 'category_id']
-        depth = 1
-
-    def calculate_tax(self, product: MenuItem):
-        return product.price * Decimal(1.1)
+        fields = ['id', 'title', 'price', 'inventory']
+        extra_kwargs = {
+            'price' : {'min_value': 2},
+            'inventory' : {'min_value': 0}
+        }
