8e25e9aa8d88154107ff165bfcbb0fd40a749ded
Mark Daniel Callejas <callejasmark63@gmail.com>
2025-07-13 12:20:00 +0800

    Add category field for POST (Write Only)

--- Django/META/LittleLemonAPI/serializers.py
+++ Django/META/LittleLemonAPI/serializers.py
@@ -12,9 +12,10 @@ class MenuItemSerializer(serializers.ModelSerializer):
     stock = serializers.IntegerField(source='inventory')
     price_after_tax = serializers.SerializerMethodField(method_name='calculate_tax')
     # category = CategorySerializer()
+    category_id = serializers.IntegerField(write_only = True)
     class Meta:
         model = MenuItem
-        fields = ['id', 'title', 'price', 'stock', 'price_after_tax', 'category']
+        fields = ['id', 'title', 'price', 'stock', 'price_after_tax', 'category', 'category_id']
         depth = 1
 
     def calculate_tax(self, product: MenuItem):
