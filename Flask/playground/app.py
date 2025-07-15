75b5060cd7173fe8bb07e3cb2540207db137427d
Mark Daniel Callejas <callejasmark63@gmail.com>
2025-07-15 23:58:21 +0800

    Flask: Add sample JSON object respond

--- Flask/playground/app.py
+++ Flask/playground/app.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return "<b>My first flask application in action!</b>"
+
+@app.route('/json')
+def json_route():
+    return {"message": "Hello World"}
