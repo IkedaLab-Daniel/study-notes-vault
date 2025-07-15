d5a335434c4444944fefcfcf718c58d35376b5e6
Mark Daniel Callejas <callejasmark63@gmail.com>
2025-07-15 22:54:57 +0800

    Django:LittleLemonAPI:Link models.py

--- Django/META/LittleLemonAPI/models.py
+++ Django/META/LittleLemonAPI/models.py
@@ -1,18 +1,21 @@
+"""Models for the Little Lemon API app."""
+
 from django.db import models
 
-# Create your models here.
 class Category(models.Model):
+    """Model representing a menu category."""
     slug = models.SlugField()
     title = models.CharField(max_length=255)
 
-    def __str__(self)-> str:
-        return self.title
+    def __str__(self) -> str:
+        return str(self.title)
 
 class MenuItem(models.Model):
+    """Model representing a menu item."""
     title = models.CharField(max_length=255)
-    price = models.DecimalField(max_digits=6 ,decimal_places=2)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.SmallIntegerField()
     category = models.ForeignKey(Category, on_delete=models.PROTECT)
 
-    def __str__(self)-> str:
-        return self.title
+    def __str__(self) -> str:
+        return str(self.title)
