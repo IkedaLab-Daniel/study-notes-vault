bf493a06c6e9782512252586cd585877bc5f770c
Mark Daniel Callejas <callejasmark63@gmail.com>
2025-09-15 22:16:24 +0800

    Refactor Babel and Webpack configurations for improved readability and structure

--- React/no-framework/babel.config.js
+++ React/no-framework/babel.config.js
@@ -1,14 +1,14 @@
 const development = (process.env.NODE_ENV || "development") === "development";
 
 module.exports = {
-    presets: [
-        [
-            "@babel/preset-react",
-            {
-                runtime: "automatic",
-                useSpread: true,
-                development,
-            }
-        ]
-    ]
-}
+  presets: [
+    [
+      "@babel/preset-react",
+      {
+        runtime: "automatic",
+        useSpread: true,
+        development: true,
+      },
+    ],
+  ],
+};
--- React/no-framework/webpack.config.js
+++ React/no-framework/webpack.config.js
@@ -6,35 +6,43 @@ const mode = process.env.NODE_ENV || "development";
 const development = mode === "development";
 
 const config = {
-    mode,
-    entry: "./src/Client.jsx",
-    module: {
-        rules: [
-            {
-                test: /\.jsx?$/,
-                use: "babel-loader",
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
-            },
-        ],
-    },
-    resolve: {
-        extensions: [".js", ".jsx"],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            inject: true,
-            publicPath: "/assets/",
-            template: "./index.html"
-        }),
-        new ReactServerWebpackPlugin({ isServer: false })
+  mode,
+  entry: "./src/Client.jsx",
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: "babel-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
     ],
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: development ? "bundle.js" : "bundle.[contenthash].js",
-        chunkFilename: development ? "[id].chunk.js" : "[id].[contenthash].js"
-    }
-}
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      publicPath: "/assets/",
+      template: "./index.html",
+    }),
+    new ReactServerWebpackPlugin({ isServer: false }),
+  ],
+  output: {
+    chunkFilename: development
+      ? "[id].chunk.js"
+      : "[id].[contenthash].chunk.js",
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+    clean: true,
+  },
+  optimization: {
+    runtimeChunk: "single",
+  },
+};
+
+module.exports = config;
