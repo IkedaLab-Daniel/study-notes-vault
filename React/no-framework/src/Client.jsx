a698a60140213ad902ca4caf2d724f4be1de5122
Mark Daniel Callejas <callejasmark63@gmail.com>
2025-09-15 22:13:18 +0800

    Enhance /react-flight response structure and improve formatting in Client and ServerComponent files

--- React/no-framework/src/Client.jsx
+++ React/no-framework/src/Client.jsx
@@ -1,10 +1,10 @@
-import { createRoot } from 'react-dom/client'
-import { createFromFetch } from 'react-server-dom-webpack/client'
+import { createRoot } from "react-dom/client";
+import { createFromFetch } from "react-server-dom-webpack/client";
 import "doodle.css/doodle.css";
 
-console. log("fetching flight response");
+console.log("fetching flight response");
 const fetchPromise = fetch("/react-flight");
-const root = createRoot (document.getElementById ("root" ));
-const p = createFromFetch(fetchPromise) ;
+const root = createRoot(document.getElementById("root"));
+const p = createFromFetch(fetchPromise);
 console.log("rendering root", p);
 root.render(p);
--- React/no-framework/src/ServerComponent.jsx
+++ React/no-framework/src/ServerComponent.jsx
@@ -1,44 +1,47 @@
 import { AsyncDatabase } from "promised-sqlite3";
-import path from "node:path"
+import path from "node:path";
 
-// > assumes that you're logged in as id=1
+// this page assumes that you are logged in as user 1
 export default async function MyNotes() {
-    console.log("rendering MyNotes server component");
-    async function fetchNotes() {
-        console.log("Running server function fetchNotes");
-        const dbPath = path.resolve(__dirname, "../../notes.db");
-        const db = await AsyncDatabase.open(dbPath);
-        const from = await db.all(
-            "SELECT n.id as id, n.note as note, f.name as from_user, t.name as to_user FROM notes n JOIN users f on f.id = n.from_user JOIN users t on t.id = n.to_user where from_user = ?", ["1"]
-        );
-        return (
-            from,
-        );
-    }
+  console.log("rendering MyNotes server component");
+  async function fetchNotes() {
+    console.log("running server function fetchNotes");
+    const dbPath = path.resolve(__dirname, "../../notes.db");
+    const db = await AsyncDatabase.open(dbPath);
+    const from = await db.all(
+      "SELECT n.id as id, n.note as note, f.name as from_user, t.name as to_user FROM notes n JOIN users f ON f.id = n.from_user JOIN users t ON t.id = n.to_user WHERE from_user = ?",
+      ["1"]
+    );
+    return {
+      from,
+    };
+  }
 
-    return (
-        <fieldset>
-            <legend>Server Component</legend>
-            <div>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>From</th>
-                            <th>To</th>
-                            <th>Note</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {notStrictEqual.from.map(({ id, note, from_user, to_user }) => {
-                            <tr key={id}>
-                                <td>{from_user}</td>
-                                <td>{to_user}</td>
-                                <td>{note}</td>
-                            </tr>
-                        })}
-                    </tbody>
-                </table>
-            </div>
-        </fieldset>
-    )
+  const notes = await fetchNotes();
+
+  return (
+    <fieldset>
+      <legend>Server Component</legend>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>From</th>
+              <th>To</th>
+              <th>Note</th>
+            </tr>
+          </thead>
+          <tbody>
+            {notes.from.map(({ id, note, from_user, to_user }) => (
+              <tr key={id}>
+                <td>{from_user}</td>
+                <td>{to_user}</td>
+                <td>{note}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </fieldset>
+  );
 }
