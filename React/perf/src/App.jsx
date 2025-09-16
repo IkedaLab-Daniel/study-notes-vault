e8fbc6a549494f72086c71d03ea909faecf4fca5
Mark Daniel Callejas <callejasmark63@gmail.com>
2025-09-17 01:01:21 +0800

    feat: implement MarkdownPreview component for rendering markdown with performance simulation

--- React/perf/src/App.jsx
+++ React/perf/src/App.jsx
@@ -1,3 +1,20 @@
+import { useEffect, useState } from "react";
+import { marked } from "marked";
+
+import MarkdownPreview from "./MarkdownPreview";
+import markdownContent from "./markdownContent";
+
 export default function App() {
-  return <h1>React Performance</h1>;
+  const [text, setText] = useState(markdownContent);
+  const [time, setTime] = useState(Date.now())
+  const [theme, setTheme] = useState("blue")
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(Date.now())
+    }, 1000)
+  }, [])
+
+  const options = { text, theme }
+  const render = (text) => marked.parse(text);
 }
