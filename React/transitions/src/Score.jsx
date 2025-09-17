d721cd66fd24c9fc33270c7ba0ba90d7e9c3c7f5
Mark Daniel Callejas <callejasmark63@gmail.com>
2025-09-18 00:01:26 +0800

    feat: add Score component with initial props setup

---
+++ React/transitions/src/Score.jsx
@@ -0,0 +1,17 @@
+const loadingUrl = "../images/loading.webp"
+
+export default function Score({
+    isPending,
+    home,
+    away,
+    awayName,
+    homeName,
+    awayImage,
+    homeImage
+}) {
+    return(
+        <div>
+            
+        </div>
+    )
+}
