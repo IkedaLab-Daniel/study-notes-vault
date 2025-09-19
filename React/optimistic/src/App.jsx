f07ffeecf3c53a276a0066d3466ec4a43d365c98
Mark Daniel Callejas <callejasmark63@gmail.com>
2025-09-19 23:22:43 +0800

    fix: correct state update in postDeepThought function and ensure proper JSON parsing in useEffect

--- React/optimistic/src/App.jsx
+++ React/optimistic/src/App.jsx
@@ -20,14 +20,37 @@ export default function App() {
     }
 
     const { thoughts: newThoughts } = await response.json();
-    setThought(newThoughts);
+    setThoughts(newThoughts);
   }
 
   useEffect(() => {
     fetch("/thoughts")
-      .then((res) => res.json)
+      .then((res) => res.json())
       .then((data) => {
         setThoughts(data);
       });
   }, [])
+
+  return (
+    <div className="app">
+      <h1>Deep Thoughts</h1>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          postDeepThought();
+        }}
+      >
+        <label htmlFor="thought">What's on your mind?</label>
+        <textarea name="thought" id="thought" rows="5" cols="33" value={thought} onChange={(e) => setThought(e.target.value)}>
+        </textarea>
+        <button type="submit">Direct my thoughts into the aether</button>
+      </form>
+
+      <ul>
+        {thoughts.map((thought, index) => (
+          <li key={index}>{thought}</li>
+        ))}
+      </ul>
+    </div>
+  )
 }
