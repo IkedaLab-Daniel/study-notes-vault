ce871963d7cece9b57fa4d0e9d24965c327dc72d
Mark Daniel Callejas <callejasmark63@gmail.com>
2025-09-19 23:13:01 +0800

    React: Optimistic: implement postDeepThought function and fetch thoughts on mount

--- React/optimistic/src/App.jsx
+++ React/optimistic/src/App.jsx
@@ -1,3 +1,33 @@
+import { useEffect, useState } from "react";
+
 export default function App() {
-  return <h1>Deep Thoughts</h1>;
+  const [thoughts, setThoughts] = useState([]);
+  const [thought, setThought] = useState("");
+
+  async function postDeepThought() {
+    setThought("");
+    const response = await fetch("/thoughts", {
+      method: "POST",
+      headers: {
+        "Content-Type" : "application/json"
+      },
+      body: JSON.stringify({ thought }),
+    });
+    
+    if (!response.ok) {
+      alert("This thought was not deep enough. Do bettter.")
+      return;
+    }
+
+    const { thoughts: newThoughts } = await response.json();
+    setThought(newThoughts);
+  }
+
+  useEffect(() => {
+    fetch("/thoughts")
+      .then((res) => res.json)
+      .then((data) => {
+        setThoughts(data);
+      });
+  }, [])
 }
