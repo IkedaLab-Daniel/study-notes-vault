d2f4d27c97878e955fe4a15378a297047455748f
Mark Daniel Callejas <callejasmark63@gmail.com>
2025-09-22 23:20:00 +0800

    Node: Added JWT protection (partially complete)

--- node/new/src/modules/auth.ts
+++ node/new/src/modules/auth.ts
@@ -6,6 +6,15 @@ type User = {
     username: string;
 };
 
+// ! Extend Express Request interface to include 'user'
+declare global {
+    namespace Express {
+        interface Request {
+            user?: string | object;
+        }
+    }
+}
+
 export const createJWT = (user: User) => {
     const secret = process.env.JWT_SECRET;
     
@@ -31,6 +40,29 @@ export const protect = (req: Request, res: Response, next: NextFunction) => {
         res.json({ "message": "no authorization header"})
         return
     }
+
+    const [, token] = bearer.split(' ');
+
+    if (!token){
+        res.status(401);
+        res.json({ "message": "why no token! ??????"})
+        return
+    }
+
+    try {
+        const secret = process.env.JWT_SECRET;
+        if (!secret) {
+            throw new Error('JWT_SECRET environment variable is not defined');
+        }
+        const user = jwt.verify(token, secret);
+        req.user = user;
+        next();
+    } catch (err){
+        console.error("invalid credentials")
+        res.status(401)
+        res.json({ "message": "invalid credentials"})
+    }
+
 
     console.log("Someone with auth tried to request")
     next()
