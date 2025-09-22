655662f7800aa7e20b857a5ef604f1eedb17229f
Mark Daniel Callejas <callejasmark63@gmail.com>
2025-09-22 23:56:38 +0800

    feat: add JWT authentication and protect API routes

--- node/new/src/modules/auth.ts
+++ node/new/src/modules/auth.ts
@@ -1,12 +1,37 @@
 import jwt from 'jsonwebtoken'
+import { Request, Response, NextFunction } from 'express';
 
-export const createJWT = (user) => {
+type User = {
+    id: string | number;
+    username: string;
+};
+
+export const createJWT = (user: User) => {
+    const secret = process.env.JWT_SECRET;
+    
+    if (!secret) {
+        throw new Error('JWT_SECRET environment variable is not defined');
+    }
+    
     // > convert object to a String
     const token = jwt.sign({
             id: user.id, 
             username: user.username
         }, 
-        process.env.JWT_SECRET
+        secret
     )
     return token
+}
+
+export const protect = (req: Request, res: Response, next: NextFunction) => {
+    const bearer = req.headers.authorization;
+
+    if (!bearer){
+        res.status(401);
+        res.json({ "message": "no authorization header"})
+        return
+    }
+
+    console.log("Someone with auth tried to request")
+    next()
 }
--- node/new/src/server.ts
+++ node/new/src/server.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import router from './router'
 import morgan from 'morgan'
 import cors from 'cors'
+import { protect } from './modules/auth';
 
 const app = express();
 
@@ -15,6 +16,6 @@ app.use((req, res, next) => {
     next()
 })
 
-app.use('/api', router)
+app.use('/api', protect, router)
 
 export default app
