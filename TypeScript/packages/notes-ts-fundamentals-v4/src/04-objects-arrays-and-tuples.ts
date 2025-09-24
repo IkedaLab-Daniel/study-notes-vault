48c23d0d5008266f1eeaa304e18f2a8dc08d0cae
Mark Daniel Callejas <callejasmark63@gmail.com>
2025-09-24 23:18:54 +0800

    TS: code: 04 Arrays

--- TypeScript/packages/notes-ts-fundamentals-v4/src/04-objects-arrays-and-tuples.ts
+++ TypeScript/packages/notes-ts-fundamentals-v4/src/04-objects-arrays-and-tuples.ts
@@ -92,60 +92,65 @@ const y = phones["work"]
 
 //*  Array Types
 
-/*
-// const fileExtensions = ["js", "ts"]
-//        ^? string[]
 
-// const cars = [ //? Let's look at an array of objects
-//     {
-//         make: "Toyota",
-//         model: "Corolla",
-//         year: 2002,
-//     },
-// ]
+const fileExtensions = ["js", "ts"]
+       // ^? string[]
+
+const cars = [ //? Let's look at an array of objects
+    {
+        make: "Toyota",
+        model: "Corolla",
+        year: 2002,
+    },
+]
 
 
 //* Tuples
-/*
-// let myCar = [
-//     2002,     // Year
-//     "Toyota", // Make
-//     "Corolla" // Model
-// ]
-// const [year, make, model] = myCar //✔️ Destructuring
+
+let myCar2 = [
+    2002,     // Year
+    "Toyota", // Make
+    "Corolla" // Model
+]
+const [year, make, model] = myCar2 //✔️ Destructuring
+
+console.log(year, make)
+
+
 
 //? Inference doesn't work very well for tuples
-/*
-// myCar = ["Honda", 2017, "Accord", "Sedan"] //! Wrong convention
-/*
-// let myCar: [number, string, string] = [
-//     2002,
-//     "Toyota",
-//     "Corolla",
-// ]
-// myCar = ["Honda", 2017, "Accord"] //! Wrong convention
-// myCar = [2017, "Honda", "Accord", "Sedan"] //! Too many elements
+
+  myCar2 = ["Honda", 2017, "Accord", "Sedan"] //! Wrong convention
+
+let myCar3: [number, string, string] = [
+    2002,
+    "Toyota",
+    "Corolla",
+]
+myCar3 = ["Honda", 2017, "Accord"] //! Wrong convention
+myCar3 = [2017, "Honda", "Accord", "Sedan"] //! Too many elements
 
 
-//*  `readonly` tuples
-/*
-// const numPair: [number, number] = [4, 5]; //✔️ Valid
-// const numTriplet: [number, number, number] = [7]; //! Invalid
+//  `readonly` tuples
 
-// [101, 102, 103].length //? number[].length
-// numPair.length //? [number, number] length
+const numPair: [number, number] = [4, 5]; //✔️ Valid
+const numTriplet: [number, number, number] = [7]; //! Invalid
 
-// numPair.push(6) // [4, 5, 6]
-// numPair.pop() // [4, 5]
-// numPair.pop() // [4]
-// numPair.pop() // []
+[101, 102, 103].length //? number[].length
+numPair.length //? [number, number] length
 
-// numPair.length  //! ❌ DANGER ❌
+numPair.push(6) // [4, 5, 6]
+numPair.pop() // [4, 5]
+numPair.pop() // [4]
+numPair.pop() // []
 
-// const roNumPair: readonly [number, number] = [4, 5]
-// roNumPair.length
-// roNumPair.push(6) // [4, 5, 6] //! Not allowed
-// roNumPair.pop() // [4, 5] //! Not allowed
+numPair.length  //! ❌ DANGER ❌
+
+const roNumPair: readonly [number, number] = [4, 5]
+roNumPair[1] = 91123; // ! not allowed
+roNumPair.length
+roNumPair.push(6) // [4, 5, 6] //! Not allowed
+roNumPair.pop() // [4, 5] //! Not allowed
 
 /**/
 
