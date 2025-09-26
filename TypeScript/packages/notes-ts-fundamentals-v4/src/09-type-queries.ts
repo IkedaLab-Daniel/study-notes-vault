601b6ed2c71bf580494e4614644fa6b1b3b3c0ff
Mark Daniel Callejas <callejasmark63@gmail.com>
2025-09-26 12:20:00 +0800

    TS: code: type of demontration

--- TypeScript/packages/notes-ts-fundamentals-v4/src/09-type-queries.ts
+++ TypeScript/packages/notes-ts-fundamentals-v4/src/09-type-queries.ts
@@ -1,19 +1,19 @@
 //* keyof
 
 type DatePropertyNames = keyof Date
-/*
-// type DateStringPropertyNames = DatePropertyNames & string
-// type DateSymbolPropertyNames = DatePropertyNames & symbol
+
+type DateStringPropertyNames = DatePropertyNames & string
+type DateSymbolPropertyNames = DatePropertyNames & symbol
 
 //* typeof
-/*
-// async function main() {
-//     const apiResponse = await Promise.all([
-//         fetch("https://example.com"),
-//         Promise.resolve("Titanium White"),
-//     ])
-//     type ApiResponseType = typeof apiResponse
-// }
+
+async function main() {
+    const apiResponse = await Promise.all([
+        fetch("https://example.com"),
+        Promise.resolve("Titanium White"),
+    ])
+    type ApiResponseType = typeof apiResponse
+}
 /*
 //?^ note: type alias within a function scope!
 // const MyAjaxConstructor = CSSRule
