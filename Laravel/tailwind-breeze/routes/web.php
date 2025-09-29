ed843df1f072d44267abf64722c30afd8e8f1655
Mark Daniel Callejas <callejasmark63@gmail.com>
2025-09-30 00:33:42 +0800

    Add tailwind-demo route

--- Laravel/tailwind-breeze/routes/web.php
+++ Laravel/tailwind-breeze/routes/web.php
@@ -7,6 +7,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/tailwind-demo', function() {
+    return view('tailwind-demo');
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
