d1fc892c48b7acf26bc070f112fff3eca2a3eccc
Mark Daniel Callejas <callejasmark63@gmail.com>
2025-10-01 23:20:00 +0800

    Laravel: Refactor dashboard view to display task details including title, description, user, and creation date.

--- Laravel/tailwind-breeze/resources/views/Tasks/dashboard.blade.php
+++ Laravel/tailwind-breeze/resources/views/Tasks/dashboard.blade.php
@@ -2,8 +2,20 @@
 
 @section('content')
     <div>
-        @foreach ($tasks as $task)
-            <p class="text-white">{{ $task }}</p>
-        @endforeach
+        <h1 class="p-4 text-5xl font-bold mb-6 text-gray-100">All tasks:</h1>
+        <div class="grid grid-cols-1 md:grid-cols-4 gap-2 p-4 text-white">
+            @foreach ($tasks as $task)
+                <div class="bg-gray-700 p-4 rounded-md">
+                    <p>Name: {{ $task->title }}</p>
+                    @if($task->title)
+                        <p>Description: {{ $task->title }}</p>
+                    @else
+                        <p>Description: No description</p>
+                    @endif
+                    <p>By: {{ $task->user->name }}</p>
+                    <p>Created at: {{ $task->created_at->format('F j, Y') }}</p>
+                </div>
+            @endforeach
+        </div>
     </div>
 @endsection
