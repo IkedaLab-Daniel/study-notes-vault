6a429c3c8eaa095e42174ca5f7a14d392eb28aeb
Mark Daniel Callejas <callejasmark63@gmail.com>
2025-10-03 01:56:25 +0800

    Update NoteSeeder to include a longer sample message and enhance index view for better note display

--- Laravel/crud-practice-2/database/seeders/NoteSeeder.php
+++ Laravel/crud-practice-2/database/seeders/NoteSeeder.php
@@ -15,7 +15,7 @@ class NoteSeeder extends Seeder
     {
         Note::create([
             'title' => 'Sample note',
-            'message' => 'this is a sample note',
+            'message' => 'this is a sample note aksjdklajs lorem ao;jksdo jasjdkalskjas kldjkjaskldj. akjslndjnasj dhrq3jnjknas dasjhdjnqwj jdnas djanbdkqwbnbdhk qwbekhndbqw kqd kjqwdb qwkbd b wqbd jqwbdbhqwgdg hqbwdhbqbwhdqbwe mnbqwbdqhbhkegduq ukqwbdkhbqksbd kuqwb wkjhdbkqbwdb. kuqwbdkbqwkdbhjqwd kqbd kqwjbdkqwkd k qwbdkhqbkhbsdna ansbdnmqw qwn,mbenqwbdas nqwjkebhqjwbbe qjksdbasbdnab bbqwneb mqwebmq wqwbemhqwbembqwmnbemnqw asn,bdsbnabnbdnmasbdnmabqwuhe bnqkwbejnqbwnas hhjqwljehjqkwbdas n,qnwbe',
             'status' => 'pending',
             'user_id' => 1
         ]);
--- Laravel/crud-practice-2/resources/views/notes/index.blade.php
+++ Laravel/crud-practice-2/resources/views/notes/index.blade.php
@@ -1,10 +1,26 @@
 <x-app-layout>
-    <body class="bg-gray-700 ">
-        @if (count($notes) == 0)
-            <h1 class="text-gray-100 text-5xl">No notes yet</h1>
-        @else
-            <h1>{{ notes }}</h1>
-        @endif
-        <h1>{{ $user_name }}</h1>
+    <body>
+        <div class="p-6">
+            @if (count($notes) == 0)
+                <div>
+                    <!-- SVG DITO MAMAYA -->
+                    <h2>No notes Yet</h2>
+                </div>
+            @else
+                <h1 class="text-slate-200 text-4xl font-bold">My notes:</h1>
+                <div class="p-4 grid grid-cols-4 gap-4">
+                    @foreach($notes as $note)
+                        <div class="p-4 text-slate-200 bg-slate-700 rounded-md">
+                            <p class="text-center text-xl font-semibold">"{{$note->title}}"</p>
+                            <div class="mt-2 border-t border-slate-400"></div>
+                            <p class="mt-3 text-sm text-slate-400">Message:</p>
+                            <p>{{$note->message}}</p>
+                        </div>        
+                    @endforeach
+                </div>
+                
+            @endif
+        
+        </div>        
     </body>
 </x-app-layout>
