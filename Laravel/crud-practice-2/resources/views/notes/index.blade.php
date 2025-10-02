6b55b7bf3f3c455db64224005b3af778f3f94039
Mark Daniel Callejas <callejasmark63@gmail.com>
2025-10-03 02:02:49 +0800

    Refactor index view layout to enhance note display and add 'Add Note' button

--- Laravel/crud-practice-2/resources/views/notes/index.blade.php
+++ Laravel/crud-practice-2/resources/views/notes/index.blade.php
@@ -7,8 +7,15 @@
                     <h2>No notes Yet</h2>
                 </div>
             @else
-                <h1 class="text-slate-200 text-4xl font-bold">My notes:</h1>
-                <div class="p-4 grid grid-cols-4 gap-4">
+                <div class="flex justify-between items-center">
+                    <span class="text-slate-200 text-4xl font-bold">My notes:</span>
+                    <div>
+                        <a href="" class="text-slate-50 bg-blue-700 py-2 px-4 rounded">
+                            Add Note
+                        </a>
+                    </div>
+                </div>
+                <div class="mt-4 md:p-4 grid grid-cols-1 md:grid-cols-4 gap-4">
                     @foreach($notes as $note)
                         <div class="p-4 text-slate-200 bg-slate-700 rounded-md">
                             <p class="text-center text-xl font-semibold">"{{$note->title}}"</p>
