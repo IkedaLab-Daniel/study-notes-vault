e115eb3dbb86143913a3a51e0928415535ca19a1
Mark Daniel Callejas <callejasmark63@gmail.com>
2025-10-05 00:13:07 +0800

    styled My Notes page rendering

--- Laravel/crud-practice-3/resources/views/Notes/index.blade.php
+++ Laravel/crud-practice-3/resources/views/Notes/index.blade.php
@@ -1,8 +1,16 @@
 <x-app-layout>
-    <h1 class="text-gray-100 text-4xl">Sample</h1>
-    @foreach($notes as $note)
-        <div class="text-gray-100">
-            <p>{{ $note->title }}</p>
+    <div class="max-w-[1200px] mx-auto">
+        <h1 class="text-gray-100 text-4xl my-4 font-semibold">My Note:</h1>
+        <div class="grid md:grid-cols-4 gap-4">
+            @foreach($notes as $note)
+                <div class="mt-4 text-gray-100 bg-gray-700 p-4 rounded-md hover:scale-105 transition-all duration-500">
+                    <p class="text-center font-bold text-2xl">"{{ $note->title }}"</p>
+                    <div class="border-t border-gray-400 my-3"></div>
+                    <p>{{ $note->message }}</p>
+                    <div class="border-t border-gray-400 my-3"></div>
+                    <p class="text-right text-sm italic">{{ $note->created_at->format('F j, Y') }}</p>
+                </div>
+            @endforeach
         </div>
-    @endforeach
+    </div>
 </x-app-layout>
