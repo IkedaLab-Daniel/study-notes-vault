1904def89d760421b1585cdaed077c89a7ae11d8
Mark Daniel Callejas <callejasmark63@gmail.com>
2025-10-05 01:55:48 +0800

    Add navigation links

--- Laravel/crud-practice-3/resources/views/layouts/navigation.blade.php
+++ Laravel/crud-practice-3/resources/views/layouts/navigation.blade.php
@@ -15,6 +15,12 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('notes.index')" :active="request()->routeIs('notes.index')">
+                        {{ __('My Notes') }}
+                    </x-nav-link>
+                    <x-nav-link :href="route('notes.create')" :active="request()->routeIs('notes.create')">
+                        {{ __('Create Note') }}
+                    </x-nav-link>
                 </div>
             </div>
 
