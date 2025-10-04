9caa03862d317c52e38fc0e1642e096336eff03e
Mark Daniel Callejas <callejasmark63@gmail.com>
2025-10-05 00:50:31 +0800

    completed UI for create new note form

--- Laravel/crud-practice-3/resources/views/Notes/create.blade.php
+++ Laravel/crud-practice-3/resources/views/Notes/create.blade.php
@@ -1,3 +1,20 @@
 <x-app-layout>
-    <h1 class="text-gray-50">Note Form will be here</h1>
+    <div class="h-full text-gray-50 bg-gray-700 w-[800px] p-4 mx-auto mt-[50px] rounded-xl shadow-lg">
+        <h1 class="text-3xl font-semibold text-center my-4">Create a new note</h1>
+        <form 
+            action="{{ route('notes.store') }}"
+            method="POST" 
+            class= "bg-gray-700 flex flex-col gap-2 px-4"
+        >
+            @csrf
+            <label class="text-xl" for="title">Title:</label>
+            <input class="bg-gray-600 w-[100%] rounded-md" type="text" name="title">
+            <label class="text-xl" for="message">Message:</label>
+            <textarea class="bg-gray-600 w-[100%] rounded-md" rows="6" name="message" id="message"></textarea>
+            <div class="mt-4 flex gap-4 ">
+                <button class="bg-gray-800 py-2 text-xl w-[50%] hover:scale-105 transition-all duration-500 shadow-lg">Back</button>
+                <button class="bg-blue-700 py-2 text-xl w-[50%] hover:scale-105 transition-all duration-500 shadow-lg">Submit</button>
+            </div>
+        </form>
+    </div>
 </x-app-layout>
