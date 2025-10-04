71f06c3d3b7a9a61562aa1bf6d9b795c2687a478
Mark Daniel Callejas <callejasmark63@gmail.com>
2025-10-04 23:51:12 +0800

    Added index method for Note controller

--- Laravel/crud-practice-3/app/Http/Controllers/NoteController.php
+++ Laravel/crud-practice-3/app/Http/Controllers/NoteController.php
@@ -10,9 +10,11 @@ class NoteController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $notes = $request->user()->notes()->latest()->get();
+
+        return view('Notes.index', compact('notes'));
     }
 
     /**
