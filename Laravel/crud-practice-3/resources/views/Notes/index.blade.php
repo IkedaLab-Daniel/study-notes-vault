535e12dd41176fb1fcd563475f7df21dc1c7ac61
Mark Daniel Callejas <callejasmark63@gmail.com>
2025-10-04 23:56:13 +0800

    added foreach loop to be used for rendering notes

--- Laravel/crud-practice-3/resources/views/Notes/index.blade.php
+++ Laravel/crud-practice-3/resources/views/Notes/index.blade.php
@@ -1,3 +1,8 @@
 <x-app-layout>
     <h1 class="text-gray-100 text-4xl">Sample</h1>
+    @foreach($notes as $note)
+        <div class="text-gray-100">
+            <p>{{ $note->title }}</p>
+        </div>
+    @endforeach
 </x-app-layout>
