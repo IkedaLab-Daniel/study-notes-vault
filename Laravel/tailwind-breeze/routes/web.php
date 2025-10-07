afc024e90ce8f688aefe436e038f41559a5afa92
Mark Daniel Callejas <callejasmark63@gmail.com>
2025-10-08 01:24:26 +0800

    Laravel: Add route for avatar uplaod

--- Laravel/tailwind-breeze/routes/web.php
+++ Laravel/tailwind-breeze/routes/web.php
@@ -22,6 +22,9 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // > for avatar uploading
+    Route::post('/profile/avatar', [ProfileController::class, 'update_avatar'])->name('profile.avatar');
 });
 
 require __DIR__.'/auth.php';
