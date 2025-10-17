f974e4fbe8c38679594369dd2c43389fc16e46a5
Mark Daniel Callejas <callejasmark63@gmail.com>
2025-10-17 12:20:00 +0800

    Django: Lab 8: Step 1

--- Django/practice-1/myproject/myapp/models.py
+++ Django/practice-1/myproject/myapp/models.py
@@ -9,9 +9,14 @@ class Drinks(models.Model):
     price = models.IntegerField() 
     category_id = models.ForeignKey(DrinkCategory, on_delete=models.PROTECT, default=None)
 
+# > Lab 7
 class Booking(models.Model):
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
     guest_count = models.IntegerField()
     reservation_time = models.DateField(auto_now=True)
     comments = models.CharField(max_length=1000)
+
+# > Lab 8
+class Employees(models.Model):
+    pass
