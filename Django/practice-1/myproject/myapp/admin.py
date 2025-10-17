96c69864b2c60fc7f14e3508e5ffa8b462529fde
Mark Daniel Callejas <callejasmark63@gmail.com>
2025-10-17 12:20:00 +0800

    Django: Lab 7: Step 3

--- Django/practice-1/myproject/myapp/admin.py
+++ Django/practice-1/myproject/myapp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Drinks, DrinkCategory
+from .models import Drinks, DrinkCategory, Booking
 
 # Register your models here.
 admin.site.register(Drinks)
 admin.site.register(DrinkCategory)
+admin.site.register(Booking)
