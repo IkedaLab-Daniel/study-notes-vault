819ca3df24878a1c15fb82c16065eec2bc9fabfd
Mark Daniel Callejas <callejasmark63@gmail.com>
2025-10-17 12:20:00 +0800

    Django: Lab 7: Complete

--- Django/practice-1/myproject/myapp/views.py
+++ Django/practice-1/myproject/myapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from myapp.forms import BookingForm
 
 # Create your views here.
 def home(request):
@@ -15,3 +16,12 @@ def drinks(request, drink_name):
     choice_of_drink = drink_name
 
     return HttpResponse(f"<h2>{drink_name} </h2>" + drinks[drink_name])
+
+def form_view(request):
+    form = BookingForm()
+    if request.method == 'POST':
+        form = BookingForm(request.POST)
+        if form.is_valid():
+            form.save()
+    context = {"form": form}
+    return render(request, "myapp/booking.html", context)
