6a369e35c4df93afbff8281e0dc69f16c1819697
Mark Daniel Callejas <callejasmark63@gmail.com>
2025-10-19 00:52:24 +0800

    Django: Lab 6: Step 1-2

--- Django/practice-1/myproject/myapp/models.py
+++ Django/practice-1/myproject/myapp/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Drinks(models.Model):
     drink_name = models.CharField(max_length=200)
     price = models.IntegerField() 
+
+class DrinkCategory(models.Model):
+    pass
