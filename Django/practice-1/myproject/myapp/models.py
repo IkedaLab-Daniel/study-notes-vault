aabfbefc92e68fdf095b65b9ab2d34a67e91d9b0
Mark Daniel Callejas <callejasmark63@gmail.com>
2025-10-19 00:46:47 +0800

    Django: Lab 4: Completed

--- Django/practice-1/myproject/myapp/models.py
+++ Django/practice-1/myproject/myapp/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 # Create your models here.
 class Drinks(models.Model):
-    drink = models.CharField(max_length=200)
+    drink_name = models.CharField(max_length=200)
     price = models.IntegerField() 
