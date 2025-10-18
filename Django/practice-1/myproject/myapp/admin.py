90630938d43895bd31872d76b9dba8363e83432f
Mark Daniel Callejas <callejasmark63@gmail.com>
2025-10-18 12:20:00 +0800

    Django: Lab 12: Init

--- Django/practice-1/myproject/myapp/admin.py
+++ Django/practice-1/myproject/myapp/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Drinks, DrinkCategory, Booking, Employees
+from .models import Drinks, DrinkCategory, Booking, Employees, Menu
 
 # Register your models here.
 admin.site.register(Drinks)
 admin.site.register(DrinkCategory)
 admin.site.register(Booking) # > Lab 7
 admin.site.register(Employees) # > Lab 8
+admin.site.register(Menu) # > Lab 11
