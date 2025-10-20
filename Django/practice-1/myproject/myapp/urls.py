15b1981ca878bb402049344c22b2c3b6bdfad23a
Mark Daniel Callejas <callejasmark63@gmail.com>
2025-10-20 19:02:45 +0800

    Django: Lab 10: Step 8-9

--- Django/practice-1/myproject/myapp/urls.py
+++ Django/practice-1/myproject/myapp/urls.py
@@ -4,4 +4,8 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('drinks/<str:drink_name>', views.drinks, name="drink_name"),
     path('booking/', views.form_view), # > Lab 7
+    
+    # > Lab 10
+    path('about', views.about, name='about'),
+    path('menu', views.menu, name='menu')
 ]
--- Django/practice-1/myproject/myapp/views.py
+++ Django/practice-1/myproject/myapp/views.py
@@ -26,10 +26,10 @@ def form_view(request):
     context = {"form": form}
     return render(request, "myapp/booking.html", context)
 
-def Menu(request):
+def menu(request):
     return HttpResponse("This is menu view")
 
-def About(request):
+def about(request):
     about_content = {
         'about': "Little Lemon is a family-owned Mediterranean restaurant, focused on traditional recipes served with a modern twist. The chefs draw inspiration from Italian, Greek, and Turkish culture and have a menu of 12–15 items that they rotate seasonally. The restaurant has a rustic and relaxed atmosphere with moderate prices, making it a popular place for a meal any time of the day."
         } 
