185154dd034e3e285fbe6bf0425b11ab759f4125
Mark Daniel Callejas <callejasmark63@gmail.com>
2025-10-22 23:49:05 +0800

    Socket: Client Script and CORS

--- Socket.io/wevdevsimplified/script.js
+++ Socket.io/wevdevsimplified/script.js
@@ -1,3 +1,19 @@
+// Import socket.io from CDN (loaded via script tag in HTML)
+const socket = io('http://localhost:3001')
+
+// Connection event listeners
+socket.on('connect', () => {
+    console.log('Connected to server! Socket ID:', socket.id)
+})
+
+socket.on('connect_error', (error) => {
+    console.error('Connection error:', error)
+})
+
+socket.on('disconnect', () => {
+    console.log('Disconnected from server')
+})
+
 const joinRoomButton = document.getElementById("room-button")
 const messageInput = document.getElementById("message-input")
 const roomInput = document.getElementById("room-input")
--- Socket.io/wevdevsimplified/server/server.js
+++ Socket.io/wevdevsimplified/server/server.js
@@ -1,5 +1,16 @@
-const io = require('socket.io')(3001)
+const io = require('socket.io')(3001, {
+    cors: {
+        origin: "*", // Allow all origins for development (use specific origins in production)
+        methods: ["GET", "POST"]
+    }
+})
 
 io.on('connection', socket => {
-    console.log(socket.id)
+    console.log('New connection:', socket.id)
+    
+    socket.on('disconnect', () => {
+        console.log('User disconnected:', socket.id)
+    })
 })
+
+console.log("Server On")
