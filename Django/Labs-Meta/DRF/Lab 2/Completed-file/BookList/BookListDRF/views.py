50a5845e571ee0e87bc7befe88de144df0a48fb4
Mark Daniel Callejas <callejasmark63@gmail.com>
2025-10-22 12:20:00 +0800

    DRF: Lab 2: Step 11-13

--- Django/Labs-Meta/DRF/Lab 2/Completed-file/BookList/BookListDRF/views.py
+++ Django/Labs-Meta/DRF/Lab 2/Completed-file/BookList/BookListDRF/views.py
@@ -4,3 +4,7 @@ from .serializers import BookSerializer
 from rest_framework import generics
 
 # Create your views here.
+class BookView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    
