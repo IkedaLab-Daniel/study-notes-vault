374ac9b69ca01cf7990b4a3b508d0c836aeeba2d
Mark Daniel Callejas <callejasmark63@gmail.com>
2025-10-24 14:46:33 +0800

    DRF: Lab 3: I was working on the wrong folder

--- Django/Labs-Meta/DRF/Lab 3/Starting-File/LittleLemonDRF/views.py
+++ Django/Labs-Meta/DRF/Lab 3/Starting-File/LittleLemonDRF/views.py
@@ -1,13 +1,1 @@
 from django.shortcuts import render
-from .models import MenuItem
-from .serializers import MenuItemSerializer
-from rest_framework import generics
-
-# Create your views here.
-class MenuItemView(generics.ListCreateAPIView):
-    queryset = MenuItem.objects.all()
-    serializer_class = MenuItemSerializer
-
-class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = MenuItem.objects.all()
-    serializer_class = MenuItemSerializer    
