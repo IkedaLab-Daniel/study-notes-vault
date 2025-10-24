9d22088116c6271c447731db3664cb64bc67a6aa
Mark Daniel Callejas <callejasmark63@gmail.com>
2025-10-24 15:27:24 +0800

    DRF: Lab 4: Step 8-9

--- Django/Labs-Meta/DRF/Lab 4/Completed-File/LittleLemonDRF/views.py
+++ Django/Labs-Meta/DRF/Lab 4/Completed-File/LittleLemonDRF/views.py
@@ -1,7 +1,10 @@
-# from rest_framework import generics
-# from .models import MenuItem, Category
-# from .serializers import MenuItemSerializer, CategorySerializer
+from rest_framework import generics
+from .models import MenuItem, Category
+from .serializers import MenuItemSerializer, CategorySerializer
 
-# class CategoriesView(generics.ListCreateAPIView):
-#     queryset = Category.objects.all()
-#     serializer_class = CategorySerializer
+class CategoriesView(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+class MenuItemView(generics.ListCreateAPIView):
+    pass
