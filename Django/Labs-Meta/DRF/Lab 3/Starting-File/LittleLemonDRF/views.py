3f0f6684e314c18e684e4c7459c17923dbde4666
Mark Daniel Callejas <callejasmark63@gmail.com>
2025-10-24 14:42:20 +0800

    DRF: Lab 3: Step 11

--- Django/Labs-Meta/DRF/Lab 3/Starting-File/LittleLemonDRF/views.py
+++ Django/Labs-Meta/DRF/Lab 3/Starting-File/LittleLemonDRF/views.py
@@ -7,3 +7,7 @@ from rest_framework import generics
 class MenuItemView(generics.ListCreateAPIView):
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer    
