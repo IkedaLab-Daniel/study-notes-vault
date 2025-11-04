27f8268f03d447a8aada79cc06dce103caeb8d23
Mark Daniel Callejas <callejasmark63@gmail.com>
2025-11-05 01:41:15 +0800

    React-TS: FCC: InputField.txt Done

--- React-TS/FreeCodeCamp/playground/src/App.tsx
+++ React-TS/FreeCodeCamp/playground/src/App.tsx
@@ -1,12 +1,16 @@
+import { useState } from 'react';
 import './App.css'
 import InputField from './components/InputField'
 
 const App: React.FC = () => {
-  
+  const [todo, setToDo] = useState<string>("");
+
+  console.log({todo});
+
   return (
     <div className='app'>
       <h2 className="heading">Taskify</h2>
-      <InputField />
+      <InputField todo={todo} setToDo={setToDo} />
     </div>
   )
 }
--- React-TS/FreeCodeCamp/playground/src/components/InputField.tsx
+++ React-TS/FreeCodeCamp/playground/src/components/InputField.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
 import './styles.css'
 
-const InputField = () => {
+interface Props{
+    todo: string;
+    setToDo: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const InputField: React.FC<Props> = ({todo, setToDo} : Props) => {
+
   return (
     <form className='input'>
-        <input type="text" placeholder='Enter a task' className='input__box'/>
+        <input 
+            type="text" 
+            placeholder='Enter a task' 
+            className='input__box'
+            value={todo}
+            onChange={(e) => setToDo(e.target.value)}
+        />
         <button className='input_submit'>Go</button>
     </form>
   )
