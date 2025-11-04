0e598731216e8add1c6309c2b7eab3f0fe5ae57f
Mark Daniel Callejas <callejasmark63@gmail.com>
2025-11-05 00:53:50 +0800

    React-TS: FCC: Project Init Style

--- React-TS/FreeCodeCamp/playground/src/App.tsx
+++ React-TS/FreeCodeCamp/playground/src/App.tsx
@@ -1,63 +1,11 @@
 import './App.css'
 
-let name: any;
-let age: number | string;
-let isStudent: boolean;
-let hobbies: string[] | boolean;
-let role:[number|string, string];
-
-let printName: (name:string) => void;
-let sum: (x:number, y:number) => number | string;
-let neva: () => never;
-
-// function printName(name: string) {
-//   console.log(name)
-// };
-// printName('Ice');
-
-
-// type Person = {
-//   name: string;
-//   day?: number;
-// };
-
-let person: Person = {
-  name: "Ice",
-  // day: 308,
-};
-
-let lotsIfPeople: Person[];
-
-let personName: unknown;
-
-
-interface Person {
-  name: string;
-  age?: number;
-};
-
-type X = {
-  a: string;
-  b: number;
-};
-
-// type X = Person & {
-//   a: string;
-//   b: number;
-// };
-
-// interface Person extends X {
-//   name: string;
-//   age?: number;
-// };
-
-function App() {
+const App: React.FC = () => {
   
-
   return (
-    <>
-      <p>Day 308 - LETSS GOOOOOOOOO!</p>
-    </>
+    <div className='app'>
+      <h2 className="heading">Taskify</h2>
+    </div>
   )
 }
 
