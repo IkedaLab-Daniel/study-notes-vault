fc0564efff1d7d4d995c7b990b8475f196546614
Mark Daniel Callejas <callejasmark63@gmail.com>
2025-11-05 01:12:52 +0800

    React-TS: FCC: InputField.tsx structure

--- React-TS/FreeCodeCamp/playground/src/App.tsx
+++ React-TS/FreeCodeCamp/playground/src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
+import InputField from './components/InputField'
 
 const App: React.FC = () => {
   
   return (
     <div className='app'>
       <h2 className="heading">Taskify</h2>
+      <InputField />
     </div>
   )
 }
