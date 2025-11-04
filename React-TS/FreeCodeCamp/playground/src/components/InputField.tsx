49142b17de91278f4ddac2b7dcbf2980de4b985c
Mark Daniel Callejas <callejasmark63@gmail.com>
2025-11-05 01:21:56 +0800

    React-TS: FCC: InputField.txt styling

--- React-TS/FreeCodeCamp/playground/src/components/InputField.tsx
+++ React-TS/FreeCodeCamp/playground/src/components/InputField.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import './styles.css'
 
 const InputField = () => {
   return (
     <form className='input'>
         <input type="text" placeholder='Enter a task' className='input__box'/>
-        <button className='input__submit'>Go</button>
+        <button className='input_submit'>Go</button>
     </form>
   )
 }
