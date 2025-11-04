d6097a556516201ac6409319e2330bb1d0958e0e
Mark Daniel Callejas <callejasmark63@gmail.com>
2025-11-05 00:44:00 +0800

    React-TS: FCC: Expanding Interface

--- React-TS/FreeCodeCamp/playground/src/App.tsx
+++ React-TS/FreeCodeCamp/playground/src/App.tsx
@@ -30,26 +30,29 @@ let lotsIfPeople: Person[];
 
 let personName: unknown;
 
+// type X = {
+//   a: string;
+//   b: number;
+// };
+
+// type Y = X & {
+//   c: string;
+//   d: number;
+// };
+
+// let y : Y = {
+//   c: "Ice",
+//   d: 308,
+// }
+
 interface Person {
   name: string;
   age?: number;
 };
 
-type X = {
-  a: string;
-  b: number;
+interface Guy extends Person {
+  profession: string;
 };
-
-type Y = X & {
-  c: string;
-  d: number;
-};
-
-let y : Y = {
-  c: "Ice",
-  d: 308,
-}
-
 
 function App() {
   
