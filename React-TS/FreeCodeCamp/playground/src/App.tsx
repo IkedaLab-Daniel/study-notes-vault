af99c15d3418a7afb626af2938d644520b753119
Mark Daniel Callejas <callejasmark63@gmail.com>
2025-11-05 00:42:07 +0800

    React-TS: FCC: Alias

--- React-TS/FreeCodeCamp/playground/src/App.tsx
+++ React-TS/FreeCodeCamp/playground/src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css'
 
-let name: string;
+let name: any;
 let age: number | string;
 let isStudent: boolean;
 let hobbies: string[] | boolean;
@@ -8,16 +8,18 @@ let role:[number|string, string];
 
 let printName: (name:string) => void;
 let sum: (x:number, y:number) => number | string;
+let neva: () => never;
 
 // function printName(name: string) {
 //   console.log(name)
 // };
 // printName('Ice');
 
-type Person = {
-  name: string;
-  day?: number;
-};
+
+// type Person = {
+//   name: string;
+//   day?: number;
+// };
 
 let person: Person = {
   name: "Ice",
@@ -25,6 +27,29 @@ let person: Person = {
 };
 
 let lotsIfPeople: Person[];
+
+let personName: unknown;
+
+interface Person {
+  name: string;
+  age?: number;
+};
+
+type X = {
+  a: string;
+  b: number;
+};
+
+type Y = X & {
+  c: string;
+  d: number;
+};
+
+let y : Y = {
+  c: "Ice",
+  d: 308,
+}
+
 
 function App() {
   
