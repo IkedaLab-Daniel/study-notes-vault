0e73763cd967c81f48f8d1cac545043e13568a6e
Mark Daniel Callejas <callejasmark63@gmail.com>
2025-11-06 00:14:17 +0800

    React-TS: FCC: Done functionality

--- React-TS/FreeCodeCamp/playground/src/components/SingleTodo.tsx
+++ React-TS/FreeCodeCamp/playground/src/components/SingleTodo.tsx
@@ -3,6 +3,7 @@ import type { Todo } from '../model'
 import { AiFillDelete, AiFillEdit } from 'react-icons/ai'
 import { MdDone } from 'react-icons/md'
 import './styles.css'
+import ToDoList from './ToDoList'
 
 type Props = {
     todo: Todo,
@@ -12,9 +13,21 @@ type Props = {
 
 const SingleTodo = ({todo, todos, setToDos}: Props) => {
 
+  const handleDone = (id: number) => {
+    setToDos(
+        todos.map((todo) => 
+            todo.id === id ? {...todo, isDone: !todo.isDone} : todo))
+  }
+
   return (
     <form className='todos__single'>
-      <span className='todos__single--text'>{todo.todo}</span>
+        {
+            todo.isDone ? (
+                <s className='todos__single--text'>{todo.todo}</s>
+            ) : (
+                <span className='todos__single--text'>{todo.todo}</span>
+            )
+        }
 
       <div>
         <span className="icon">
@@ -23,7 +36,7 @@ const SingleTodo = ({todo, todos, setToDos}: Props) => {
         <span className="icon">
             <AiFillDelete />
         </span>
-        <span className="icon">
+        <span className="icon" onClick={() => handleDone(todo.id)}>
             <MdDone />
         </span>
       </div>
