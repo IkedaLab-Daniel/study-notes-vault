2a3743ed49fea9969c466893d29594194699ca55
Mark Daniel Callejas <callejasmark63@gmail.com>
2025-11-10 01:22:45 +0800

    MongoDB: JS: Basic

---
+++ MongoDB/JS/app.js
@@ -0,0 +1,43 @@
+const { error } = require('console')
+const { connect } = require('http2')
+const { MongoClient } = require('mongodb')
+require('dotenv').config()
+
+const uri = process.env.MONGODB_URI
+const client = new MongoClient(uri)
+
+const dbname = "jsmongo"
+const collection_name = "accounts"
+
+const accountCollection = client.db(dbname).collection(collection_name)
+
+// > connect to db
+const connectToDatabase = async () => {
+    try {
+        await client.connect()
+        console.log(`Connected to ${dbname} database - Full string: ${uri}`)
+    } catch (error){
+        console.error( `Error connecting to database: ${error}`)
+    }
+}
+
+const sampleAccount = {
+    account_holder: "Ice",
+    account_id: "iceice",
+    account_type: "checking",
+    aura: 999999,
+    last_updated: new Date(),
+}
+
+const main = async () => {
+    try {
+        await connectToDatabase()
+        console.log("Working")
+    } catch (err) {
+        console.error("Error on main: ", err)
+    } finally {
+        await client.close()
+    }
+}
+
+main()
