bed22ebebb78cef4776f6897fe05d252fdf208e8
Mark Daniel Callejas <callejasmark63@gmail.com>
2025-11-29 14:12:33 +0800

    NLP: JS: NaiveBayes Training - Preprocess

--- NLP/JavaScript/NaiveBayesRealData.js
+++ NLP/JavaScript/NaiveBayesRealData.js
@@ -1,7 +1,14 @@
 const natural = require('natural')
 const classifier = new natural.BayesClassifier()
-
 const fs = require('node:fs')
+
+const preprocess = s => (s || '')
+    .toLowerCase()
+    .replace(/https?:\/\/\S+/gi, '')
+    .replace(/@\w+/g, '')
+    .replace(/[#]/g, '')
+    .replace(/\s+/g, ' ')
+    .trim()
 
 fs.readFile('./twitter_samples/negative_tweets.json', 'utf-8', (err, data) => {
     if (err) {
