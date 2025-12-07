d686a50aa54f8aca0ba96037960ae59f5c4f3807
Mark Daniel Callejas <callejasmark63@gmail.com>
2025-12-07 21:28:37 +0800

    Add Shadcn Button

---
+++ Shadcn/postcss.config.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  plugins: {
+    // Tailwind v4 uses a separate PostCSS plugin package
+    // Install with: npm install -D @tailwindcss/postcss
+    '@tailwindcss/postcss': {},
+    autoprefixer: {},
+  },
+}
