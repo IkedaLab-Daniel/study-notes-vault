b30dae6b66a1dd941a0c56df677634e7c65cc4ed
Ikedalab-Daniel <callejasmark63@gmail.com>
2025-12-15 01:51:20 +0800

    RAG: Code: Langchain Finalized

--- RAG/Codes/01-langchain.py
+++ RAG/Codes/01-langchain.py
@@ -1,13 +1,15 @@
 # import warnings # ? Ignore Warning
 # warnings.filterwarnings("ignore", category=UserWarning, module="urllib3")
-
+from random import randint
 from langchain_ollama import OllamaLLM
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
 
 # > Step 1 - Connect to local LLM
+model = "gemma3:1b"
+
 llm = OllamaLLM(
-    model="gemma3:1b",
+    model=model,
     temperature=0.2,
 )
 
@@ -15,8 +17,8 @@ llm = OllamaLLM(
 prompt = PromptTemplate(
     input_variables=["task"],
     template="""
-You are a helpful coding assistant.
-Write clean, well-commented code.
+You are a helpful coding assistant for HTML 
+Add CSS <style> and JS <script> on the same file (HTML) if needed.
 
 Task:
 {task}
@@ -32,16 +34,29 @@ chain = LLMChain(
 )
 
 # > Step 4 - Run Chain
+user_prompt = input("Enter code generation request: ")
 result = chain.run(
-    task="Python function that checks if a number is prime"
+    task=user_prompt
 )
 
+# > Step 5 - HTML file creation
+    #  > Remove code block markers if present
+if result.startswith("```html"):
+    result = result[len("```html"):].lstrip()
+if result.endswith("```"):
+    result = result[:-3].rstrip()
+
+    # > Save HTML
+filename = "./generations/Output" + str((randint(1,1000))) + ".html"
+with open(filename, "x") as file:
+    print(f"{filename} successfully created...")
+with open(filename, "w") as file:
+    file.write(result)
+
 print(result)
-
-
-
-print("""\033[92m
+print(f"""\033[92m
     |-----------------------------------------|
-    |  Code execution completed successfully  |
+    >> Code execution completed successfully <<
+      >>  Code: {filename} <<
     |-----------------------------------------|
 \033[0m""")
