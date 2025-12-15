8618334bdf0d8966828eb59748703aac80b1d447
Ikedalab-Daniel <callejasmark63@gmail.com>
2025-12-16 01:48:09 +0800

    RAG: Code: 03-RAG - Imports

---
+++ RAG/Codes/03-RAG.py
@@ -0,0 +1,12 @@
+from langchain_community.document_loaders import TextLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.embeddings import OllamaEmbeddings
+from langchain_community.vectorstores import faiss
+from langchain_community.llms import ollama
+from langchain.chains import retrieval_qa
+
+print(f"""\033[92m
+    |-----------------------------------------|
+    >> Code execution completed successfully <<
+    |-----------------------------------------|
+\033[0m""")
