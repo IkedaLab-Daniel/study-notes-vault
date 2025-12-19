84fd905fc724a568dd777e7ed0d60ce04b481681
Ikedalab-Daniel <callejasmark63@gmail.com>
2025-12-19 12:20:00 +0800

    RAG: Code 05 Helper - Download File

---
+++ RAG/Codes/Download_File.py
@@ -0,0 +1,8 @@
+import wget
+
+filename = 'companyPolicies.txt'
+url = 'https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/6JDbUb_L3egv_eOkouY71A.txt'
+
+# Use wget to download the file
+wget.download(url, out=filename)
+print('file downloaded')
