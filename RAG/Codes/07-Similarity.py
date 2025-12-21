ac5f64655cc24dc402dcdc6a3adfff7473f2e095
Ikedalab-Daniel <callejasmark63@gmail.com>
2025-12-22 00:39:30 +0800

    RAG: Code 07 Manual L2 implementation

--- RAG/Codes/07-Similarity.py
+++ RAG/Codes/07-Similarity.py
@@ -18,6 +18,19 @@ model = SentenceTransformer('paraphrase-MiniLM-L6-v2')
 
 # > Generate Embeddings
 embeddings = model.encode(documents)
-print(embeddings.shape)
+print("\033[92m\n\n")
+# print(embeddings.shape)
+# print(embeddings)
+
+# > Manual implementation of L2 distance calculation
+def euclidean_distance_fn(vector1, vector2):
+    squared_sum = sum((x - y) ** 2 for x, y in zip(vector1, vector2))
+    return math.sqrt(squared_sum)
+
+print(f"\nEuclidean Distance for \n >> {documents[0]}\n >> {documents[1]}")
+print(euclidean_distance_fn(embeddings[0], embeddings[1]))
+
+print(f"\nEuclidean Distance for \n >> {documents[1]}\n >> {documents[2]}")
+print(euclidean_distance_fn(embeddings[1], embeddings[2]))
 
 print("-- End --")
