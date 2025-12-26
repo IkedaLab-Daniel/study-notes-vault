3c36c1db96e4e31df43c15e27cdee51abb6fbeca
Ikedalab-Daniel <callejasmark63@gmail.com>
2025-12-27 02:04:05 +0800

    RAG: Code: 11 - Text Splitter

--- RAG/Codes/11-LangchainRetrievers.py
+++ RAG/Codes/11-LangchainRetrievers.py
@@ -44,7 +44,18 @@ def llm():
     watsonxLLM = WatsonxLLM(model=model)
     return watsonxLLM
 
-llm()
+# > Use the Text Splitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+def text_splitter(data, chunk_size, chunk_overlap):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=chunk_size,
+        chunk_overlap=chunk_overlap,
+        length_function=len,
+    )
+
+    chunk = text_splitter.split_text(data)
+    return chunk
 
 print("\033[92m --- END --- ")
 
