5f4140982a83beda7d6dff5ffba1d1f07366ea1d
Ikedalab-Daniel <callejasmark63@gmail.com>
2025-12-28 02:51:07 +0800

    RAG: Code - 12 Init

---
+++ RAG/Codes/12-LlamaIndexRetrievers.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_env
+import json
+import asyncio
+import warnings
+import numpy as np
+from typing import List, Optional
+
+warnings.filterwarnings('ignore')
+load_env()
+
