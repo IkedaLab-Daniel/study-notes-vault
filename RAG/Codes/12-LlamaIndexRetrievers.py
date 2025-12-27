c36ab9fe7c8e73b104f9ed221d101ae5ed9ecac0
Ikedalab-Daniel <callejasmark63@gmail.com>
2025-12-28 02:56:52 +0800

    RAG: Code - 12 LlamaIndex imports

--- RAG/Codes/12-LlamaIndexRetrievers.py
+++ RAG/Codes/12-LlamaIndexRetrievers.py
@@ -9,3 +9,32 @@ from typing import List, Optional
 warnings.filterwarnings('ignore')
 load_env()
 
+# > Core LlamaIndex imports
+from llama_index.core import (
+    VectorStoreIndexm,
+    SimpleDirectoryReader,
+    Document,
+    Settings,
+    DocumentSummaryIndex,
+    KeywordTableIndex
+)
+
+from llama_index.core.retrievers import (
+    BaseRetriever,
+    VectorIndexRetriever,
+    AutoMergingRetriever,
+    RecursiveRetriever,
+    QueryFusionRetriever
+)
+
+from llama_index.core.indices.document_summary import (
+    DocumentSummaryIndexLLMRetriever,
+    DocumentSummaryIndexEmbeddingRetriever
+)
+
+from llama_index.core.node_parser import SentenceSplitter, HierarchicalNodeParser
+from llama_index.core.schema import NodeWithScore, QueryBundle
+from llama_index.core.query_engine import RetrieverQueryEngine
+from llama_index.core.postprocessor import SentenceTransformerRerank
+from llama_index.core.embeddings import BaseEmbedding
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
