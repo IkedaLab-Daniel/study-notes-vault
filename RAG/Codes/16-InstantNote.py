1c0b131ff635cd837f774ccf91eeef085cc0495b
Ikedalab-Daniel <callejasmark63@gmail.com>
2026-01-10 12:20:00 +0800

    RAG: Code 16 - Integrating LLM -> import packages needed, setup parameter

--- RAG/Codes/16-InstantNote.py
+++ RAG/Codes/16-InstantNote.py
@@ -58,5 +58,23 @@ iface = gr.Interface(
     api_name=False,
 )
 
-
 iface.launch(server_name="0.0.0.0", server_port=5005)
+
+# > Integrating LLM using IBM watsonx Granite
+from ibm_watsonx_ai.foundation_models.utils.enums import ModelTypes # ? Specifying model types
+from ibm_watsonx_ai import APIClient, Credentials # ? for client and credentials parameters
+from ibm_watsonx_ai.metanames import GenTextParamsMetaNames as GenParam # ? For managing model parameters
+from ibm_watsonx_ai.foundation_models.utils.enums import DecodingMethods # ? for defining decoding methods
+from langchain_ibm import WatsonxLLM, WatsonxEmbeddings # ? For interacting with IBM's LLM and embedding
+from ibm_watsonx_ai.foundation_models.utils import get_embedding_model_specs # ? for retrieving model specification
+from langchain.chains import LLMChain # ? For creating chains of operations with LLMs
+from langchain.prompts import PromptTemplate # ? For defining prompt templates
+
+parameters = {
+    GenParam.DECODING_METHOD: "sample",
+    GenParam.MAX_NEW_TOKENS: 512,
+    GenParam.MIN_NEW_TOKENS: 1,
+    GenParam.TEMPERATURE: 0.5,
+    GenParam.TOP_K: 50,
+    GenParam.TOP_P: 1,
+}
