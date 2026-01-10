8819c374e7aa8cf7b7979619704f13aea7b63a00
Ikedalab-Daniel <callejasmark63@gmail.com>
2026-01-10 12:20:00 +0800

    RAG: Code 16 - Refactor audio transcription functionality and set up Gradio interface

--- RAG/Codes/16-InstantNote.py
+++ RAG/Codes/16-InstantNote.py
@@ -23,37 +23,40 @@ def download_sample_audio():
 download_sample_audio()
 
 # > Implement OpenAI Whisper for transcribing
-import torch
 from transformers import pipeline
 
-pipe = pipeline(
-    "automatic-speech-recognition",
-    model="openai/whisper-tiny.en",
-    chunk_length_s=30,
-)
+def transcript_audio(audio_file):
+    pipe = pipeline(
+        "automatic-speech-recognition",
+        model="openai/whisper-tiny.en",
+        chunk_length_s=30,
+    )
 
-sample = 'sample-meeting.wav'
+    # ? Perform speech recognition on the audio file
+    prediction = pipe(audio_file, batch_size=8)["text"]
 
-# ? Perform speech recognition on the audio file
-prediction = pipe(sample, batch_size=8)["text"]
+    if prediction:
+        print("\033[92m\nOK ( ˶ˆᗜˆ˵ ) >> Audio successfully transcribed: \033[0m")
+        print(prediction)
+        return prediction
+    else:
+        print("\033[91m\nERROR (╥﹏╥) >> Failed to transcribe audio sample\033[0m")
+        return "Error transcribing the audio"
 
-if "ello" in prediction:
-    print("\033[92m\n( ˶ˆᗜˆ˵ ) HEYYAA! \033[0m")
-
-if prediction:
-    print("\033[92m\nOK ( ˶ˆᗜˆ˵ ) >> Audio successfully transcribed: \033[0m")
-    print(prediction)
-else:
-    print("\033[91m\nERROR (╥﹏╥) >> Failed to transcribe audio sample\033[0m")
-
-# > Gradio Interface
+# > Set up Gradio interface < #
 import gradio as gr
 
-def greet(name):
-    return "Hello " + name + "!"
+iface = gr.Interface(
+    fn=transcript_audio,
+    inputs=gr.Audio(
+        sources=["microphone", "upload"],
+        type="filepath"
+    ),
+    outputs=gr.Textbox(),
+    title="Audio Transcription App",
+    description="Upload an audio file to transcribe it to text",
+    api_name=False,
+)
 
-demo = gr.Interface(fn=greet, inputs="text", outputs="text")
 
-demo.launch(server_name="0.0.0.0", server_port=5002)
-
-print("\033[92mEND (ᵔ ᵕ ᵔ˶)\033[0m")
+iface.launch(server_name="0.0.0.0", server_port=5005)
