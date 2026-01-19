3c7e22f9601803c89b81659086bfc498cef45a11
Ikedalab-Daniel <callejasmark63@gmail.com>
2026-01-19 12:20:00 +0800

    RAG: IBM: C6 - Added Wikipedia and minimal UI

--- RAG/Codes/21-SimpleAgent.py
+++ RAG/Codes/21-SimpleAgent.py
@@ -1,7 +1,9 @@
 from langgraph.prebuilt import create_react_agent
 from langchain_ollama import ChatOllama
 from langchain_core.tools import tool
+from langchain_community.utilities import WikipediaAPIWrapper
 import re
+from utils import print_agent
 
 llm = ChatOllama(model="llama3.2:3b")
 
@@ -54,13 +56,34 @@ def divide_numbers(dividend: str, divisor: str) -> float:
         return "Error: Cannot divide by zero"
     return div1 / div2 
 
+@tool
+def search_wikipedia(query: str) -> str:
+    """
+    Search Wikipedia for information about a topic.
+    
+    Args:
+        query: The topic or question to search for on Wikipedia
+    
+    Returns:
+        A summary of the Wikipedia article
+    """
+    wikipedia = WikipediaAPIWrapper()
+    return wikipedia.run(query)
+
+tools = [add_numbers, multiply_numbers, divide_numbers, search_wikipedia, search_wikipedia]
+
 math_agent = create_react_agent(
     model=llm,
-    tools=[add_numbers, multiply_numbers, divide_numbers],
-    prompt="You are a helpful math assistant that can perform operations such as addition, multiplication, and division"
+    tools=tools,
+    prompt="You are a helpful assistant that can perform math operations (addition, multiplication, division) and search Wikipedia for information. You can also search on Wikipedia searching information."
 )
 
-query = input("Enter query: ")
+print("""\033[34m 
+    |-------------------------------|
+    |          Math Agent           |
+    |-------------------------------|
+""")
+query = input("\033[37m   >> Enter query: ")
 
 response = math_agent.invoke(
     {"messages": [("human", query)]}
@@ -68,4 +91,14 @@ response = math_agent.invoke(
 
 final_answer = response["messages"][-1].content
 
-print(final_answer)
+print(f"""\033[34m 
+    |------------------------ Query ------------------------|
+        >> {query}           
+    |-------------------------------------------------------|
+""")
+print_agent()
+print(f"""\033[92m
+|-------------------------------------------------------------|
+>> {final_answer}
+|-------------------------------------------------------------|
+       """)
