0f59a4f61c5e486d42b5895505b6e69c4ef8b0da
Ikedalab-Daniel <callejasmark63@gmail.com>
2026-02-09 12:20:00 +0800

    Remove unused import and enhance CSV listing tool with additional print statements for better clarity

--- RAG & Agentic AI/Codes/23-LCEL-Agent.py
+++ RAG & Agentic AI/Codes/23-LCEL-Agent.py
@@ -4,7 +4,6 @@ import matplotlib
 import seaborn
 import sklearn
 import langchain
-import openai
 import langchain_openai
 
 import glob
@@ -28,3 +27,13 @@ def list_csv_files() -> Optional[List[str]]:
     if not csv_files:
         return None
     return [os.path.basename(file) for file in csv_files]
+
+print("Tool Name:", list_csv_files.name)
+print("Tool Description:", list_csv_files.description)
+print("Tool Arguments:", list_csv_files.args)
+
+groq_llm = langchain_openai.ChatOpenAI(
+     model="llama-3.1-8b-instant",  # Fast, reliable, good for function calling
+     api_key=os.getenv('GROQ_API'),  # Get from https://console.groq.com
+     base_url="https://api.groq.com/openai/v1"
+ )
