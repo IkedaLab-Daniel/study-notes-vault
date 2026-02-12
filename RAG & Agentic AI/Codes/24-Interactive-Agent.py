a9829be20f9a3b7a9fe9cf20afe89523e264839d
Ikedalab-Daniel <callejasmark63@gmail.com>
2026-02-13 00:10:11 +0800

    RAG & Agentic AI: Code 24 - add tool, tool set, llm with tools init

--- RAG & Agentic AI/Codes/24-Interactive-Agent.py
+++ RAG & Agentic AI/Codes/24-Interactive-Agent.py
@@ -8,10 +8,29 @@ load_dotenv()
 ### -- LLM Setup -- ###
 from langchain.chat_models import init_chat_model
 from langchain_openai import ChatOpenAI
-llm = init_chat_model("gpt-4o-mini", model_provider="openai")
+llm = init_chat_model("llama-3.1-8b-instant", model_provider="meta")
 
 groq_llm = ChatOpenAI(
      model="llama-3.1-8b-instant",  # Fast, reliable, good for function calling
      api_key=os.getenv('GROQ_API'),  # Get from https://console.groq.com
      base_url="https://api.groq.com/openai/v1"
  )
+
+### -- Defining an add functiuon -- ###
+@tool
+def add(a: int, b: int) -> int:
+    """
+    Add a and b
+    
+    :param a: first integer to be added
+    :type a: int
+    :param b: second integer to be added
+    :type b: int
+    :return: sum of a and b
+    :rtype: int
+    """
+    return a + b
+
+tools = [add]
+
+llm_with_tools = groq_llm.bind_tools(tools)
