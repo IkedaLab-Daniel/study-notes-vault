8628a56ebbfeef818a68c8035e92deaed22d494d
Ikedalab-Daniel <callejasmark63@gmail.com>
2026-02-13 00:50:15 +0800

    RAG & Agentic AI: Code 24 - Building an Agent

--- RAG & Agentic AI/Codes/24-Interactive-Agent.py
+++ RAG & Agentic AI/Codes/24-Interactive-Agent.py
@@ -120,3 +120,42 @@ answer = llm_with_tools.invoke(chat_history)
 print_agent()
 print(type(answer))
 print(answer.content)
+
+### -- Building an Agent -- ###
+class ToolCallingAgent:
+    def __init__(self, llm):
+        self.llm_wuth_tools = llm.bind_tools(tools)
+        self.tool_map = tool_map
+    
+    def run(self, query: str) -> str:
+        # > Step 1: Initial user message
+        chat_history = [HumanMessage(content=query)]
+
+        # > Step 2: LLM Chooses tool
+        response = self.llm_wuth_tools.invoke(chat_history)
+        if not response.tool_calls:
+            return response.content
+
+        # > Step 3: Handle first tool call
+        tool_call = response.tool_calls[0]
+        tool_name = tool_call["name"]
+        tool_args = tool_call["args"]
+        tool_call_id = tool_call["id"]
+
+        # > Step 4: Call tool manually
+        tool_result = self.tool_map[tool_name].invoke(tool_args)
+
+        # > Step 5: Send result back to LLM
+        tool_message = ToolMessage(content=str(tool_result), tool_call_id=tool_call_id)
+        chat_history.extend([response, tool_message])
+
+        # > Step 6: Fianl LLM Result
+        final_response = self.llm_wuth_tools.invoke(chat_history)
+        return final_response.content
+
+my_agent = ToolCallingAgent(llm)
+
+print_agent()
+print(my_agent.run("one plus 2"))
+print(my_agent.run("one - 2"))
+print(my_agent.run("three times two"))
