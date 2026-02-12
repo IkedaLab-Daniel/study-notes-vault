238277ce6998e7e6c50f9854eafaa32505a67064
Ikedalab-Daniel <callejasmark63@gmail.com>
2026-02-13 00:29:00 +0800

    RAG & Agentic AI: Code 24 - Testing the functions

--- RAG & Agentic AI/Codes/24-Interactive-Agent.py
+++ RAG & Agentic AI/Codes/24-Interactive-Agent.py
@@ -1,6 +1,7 @@
 from langchain_core.tools import tool
 from langchain_core.messages import HumanMessage, ToolMessage
 from dotenv import load_dotenv
+from utils import print_agent
 import os
 
 load_dotenv()
@@ -8,13 +9,11 @@ load_dotenv()
 ### -- LLM Setup -- ###
 from langchain.chat_models import init_chat_model
 from langchain_openai import ChatOpenAI
-llm = init_chat_model("llama-3.1-8b-instant", model_provider="meta")
-
-groq_llm = ChatOpenAI(
-     model="llama-3.1-8b-instant",  # Fast, reliable, good for function calling
-     api_key=os.getenv('GROQ_API'),  # Get from https://console.groq.com
-     base_url="https://api.groq.com/openai/v1"
- )
+llm = init_chat_model(
+    "llama-3.1-8b-instant", 
+    model_provider="groq",
+    api_key=os.getenv('GROQ_API')
+)
 
 ### -- Defining an add functiuon -- ###
 @tool
@@ -33,4 +32,47 @@ def add(a: int, b: int) -> int:
 
 tools = [add]
 
-llm_with_tools = groq_llm.bind_tools(tools)
+llm_with_tools = llm.bind_tools(tools)
+
+### -- More tools --- ###
+@tool
+def subtract(a: int, b: int) -> int:
+    """
+    Subtract b from a
+    
+    :type a: int
+    :type b: int
+    :rtype: int
+    """
+    return a - b
+
+@tool
+def multiply(a: int, b: int) -> int:
+    """
+    Multiply a and b
+
+    :type a: in
+    :type b: int
+    :rtype: int
+    """
+    return a * b
+
+### -- Testing the functions -- ###
+# tool_map = {
+#     "add": add,
+#     "subtract": subtract,
+#     "multiply": multiply
+# }
+
+# input_ = {
+#     "a": 1,
+#     "b": 2
+# }
+
+# result = tool_map["add"].invoke(input_)
+# print_agent()
+# print(result)
+
+result = add.invoke({ "a": 123123, "b": 190238921})
+print_agent()
+print(result)
