e34b3f3f5c33d7a633d63100fb9a761e923fa9eb
Ikedalab-Daniel <callejasmark63@gmail.com>
2026-02-17 22:40:02 +0800

    RAG & Agentic AI: Code 26 - Tasks

--- RAG & Agentic AI/Codes/26-DataVisualizationAgent.py
+++ RAG & Agentic AI/Codes/26-DataVisualizationAgent.py
@@ -53,5 +53,33 @@ agent = create_pandas_dataframe_agent(
 # print_agent()
 # print("     >>",response.content)
 
+# ! NOTE: This wont work on terminal. I'm executing the code on Jupyter Notebook.
+
 response = agent.invoke("How many rows of data are in this file?")
 print(response['output'])
+
+### -- Checking -- ###
+code_done = response['intermediate_steps'][-1][0].tool_input.replace('; ', '\n')
+print(code_done)
+
+# ? Test
+response = agent.invoke("Give me all the data where student's age is over 18 years old.")
+code_done = response['intermediate_steps'][-1][0].tool_input.replace('; ', '\n')
+print(code_done)
+
+### -- Plot data with natural language -- ###
+# ? Task 1
+response = agent.invoke("Generate a bar chart to plot the gender count.")
+print(response['intermediate_steps'][-1][0].tool_input.replace('; ', '\n'))
+
+# ? Task 2
+response = agent.invoke("Generate a pie chart to display average value of Walc for each Gender.")
+print(response['intermediate_steps'][-1][0].tool_input.replace('; ', '\n'))
+
+# ? Task 3
+response = agent.invoke("Create box plots to analyze the relationship between 'freetime' (amount of free time) and 'G3' (final grade) across different levels of free time.")
+print(response['intermediate_steps'][-1][0].tool_input.replace('; ', '\n'))
+
+# ? Task 4
+response = agent.invoke("Generate scatter plots to examine the correlation between 'Dalc' (daily alcohol consumption) and 'G3', and between 'Walc' (weekend alcohol consumption) and 'G3'.")
+print(response['intermediate_steps'][-1][0].tool_input.replace('; ', '\n'))
