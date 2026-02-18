e04d2c2dc86b4aa6a20928fd1226487fcbe0d332
Ikedalab-Daniel <callejasmark63@gmail.com>
2026-02-18 11:00:00 +0800

    RAG & Agentic AI: Project - SQL Agent: sql_agent file

---
+++ RAG & Agentic AI/Projects/SQL_Agent/sql_agent.py
@@ -0,0 +1,36 @@
+def warn(*arg, **kwwargs):
+    pass
+import warnings
+warnings.warn = warn
+warnings.filterwarnings('ignore')
+
+from langchain_groq import ChatGroq
+from langchain_community.utilities.sql_database import SQLDatabase
+from langchain_community.agent_toolkits import create_sql_agent
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+GROQ_API=os.getenv('GROQ_API')
+
+llm = ChatGroq(
+    model="llama-3.1-8b-instant",
+    temperature=0.2,
+    api_key=GROQ_API,
+    max_tokens=1024
+)
+
+mysql_username = 'root'  # Replace with your server connect information
+mysql_password = 'ikedalab123' # ! This is just placeholder, not my password :P
+mysql_host = '127.0.0.1' # Replace with your server connect information
+mysql_port = '3306' # Replace with your server connect information
+database_name = 'Chinook'
+mysql_uri = f'mysql+mysqlconnector://{mysql_username}:{mysql_password}@{mysql_host}:{mysql_port}/{database_name}'
+db = SQLDatabase.from_uri(mysql_uri)
+
+agent_executor = create_sql_agent(llm=llm, db=db, verbose=True, handle_parsing_errors=True)
+
+agent_executor.invoke(
+    "How many Album are there in the database?"
+)
