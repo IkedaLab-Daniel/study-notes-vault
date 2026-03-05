80594b2275178e4d0fd29a69041fc34076b30cae
Ikedalab-Daniel <callejasmark63@gmail.com>
2026-03-05 10:00:00 +0800

    Django: Playground 1 - Lab 2: setup urls, views

--- Django/playground/1_environment/myproject/myapp/views.py
+++ Django/playground/1_environment/myproject/myapp/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    html = """
+    <div>
+        <h1 style="color: red;">Hello</h1>
+    </div>
+"""
+    return HttpResponse(html)
