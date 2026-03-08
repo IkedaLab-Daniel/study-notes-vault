c7bfff9558286434ab8f83fab01e44199c2f9b21
Ikedalab-Daniel <callejasmark63@gmail.com>
2026-03-08 23:38:38 +0800

    Django: Lab 8 - Using Django Admin

--- Django/playground/1_environment/myproject/myapp/models.py
+++ Django/playground/1_environment/myproject/myapp/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 
-class Booking(models.Model):
+class Employees(models.Model):
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
-    guest_count = models.IntegerField()
-    reservation_time = models.DateField(auto_now=True)
-    comments = models.CharField(max_length=1000)
+    role = models.CharField(max_length=200)
+    shift = models.IntegerField()
+
+    def __str__(self):
+        return self.first_name
--- Django/playground/1_environment/myproject/myapp/urls.py
+++ Django/playground/1_environment/myproject/myapp/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
-from .views import form_view
 
 urlpatterns = [
-    path('', view=form_view, name="form_view"),
+    
 ]
