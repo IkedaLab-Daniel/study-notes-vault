941da348cd95f989dc223911c9cbe9c149449a65
Ikedalab-Daniel <callejasmark63@gmail.com>
2026-03-08 23:53:52 +0800

    Django: Lab 10 - Creating Templates - p1

--- Django/playground/1_environment/myproject/myapp/views.py
+++ Django/playground/1_environment/myproject/myapp/views.py
@@ -1,1 +1,9 @@
 from django.shortcuts import render
+
+def Menu(request):
+   pass
+
+def About(request):
+   about_content = {'about': "Little Lemon is a family-owned Mediterranean restaurant, focused on traditional recipes served with a modern twist. The chefs draw inspiration from Italian, Greek, and Turkish culture and have a menu of 12–15 items that they rotate seasonally. The restaurant has a rustic and relaxed atmosphere with moderate prices, making it a popular place for a meal any time of the day."} 
+
+   render(request, "page.html", {'content': about_content})
