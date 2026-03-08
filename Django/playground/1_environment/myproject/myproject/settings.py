b34e63996c97107fec3caae9be64833f4e225479
Ikedalab-Daniel <callejasmark63@gmail.com>
2026-03-08 23:48:12 +0800

    Django: Lab 9 - Connecting to a Database

--- Django/playground/1_environment/myproject/myproject/settings.py
+++ Django/playground/1_environment/myproject/myproject/settings.py
@@ -75,19 +75,19 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-    # ? Local MySQL Server
     # 'default': {
-    #     'ENGINE': 'django.db.backends.mysql',
-    #     'NAME': 'djangopractice',
-    #     'USER': 'root',
-    #     'PASSWORD': 'ikedalab123',
-    #     'HOST': '127.0.0.1',
-    #     'PORT': '3306',
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
+    # ? Local MySQL Server
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'feedback',
+        'USER': 'root',
+        'PASSWORD': 'ikedalab123', # ! This is just sample password
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
 }
 
 
