b1f5465286b7f92dca022bbc5fdb46542c2fad20
Ikedalab-Daniel <callejasmark63@gmail.com>
2026-03-11 04:36:49 +0800

    Django: Lab 10 - Creating Templates p3

--- Django/playground/1_environment/myproject/myapp/models.py
+++ Django/playground/1_environment/myproject/myapp/models.py
@@ -1,10 +1,3 @@
 from django.db import models
 
-class Employees(models.Model):
-    first_name = models.CharField(max_length=200)
-    last_name = models.CharField(max_length=200)
-    role = models.CharField(max_length=200)
-    shift = models.IntegerField()
-
-    def __str__(self):
-        return self.first_name
+# Create your models here.
--- Django/playground/1_environment/myproject/myapp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from myapp.views import Menu, About
-
-urlpatterns = [
-    path('about', view=About, name="about"),
-    path('menu', view=Menu, name="Menu")
-]
