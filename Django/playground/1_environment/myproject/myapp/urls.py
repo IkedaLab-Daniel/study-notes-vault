43479d941c449143327f6dad70113e76ece21448
Ikedalab-Daniel <callejasmark63@gmail.com>
2026-03-11 22:46:46 +0800

    Django: Lab 12 - Working with Template Inheritance - done

--- Django/playground/1_environment/myproject/myapp/urls.py
+++ Django/playground/1_environment/myproject/myapp/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from myapp.views import menu
+from . import views
 
 urlpatterns = [
-    path('menu', view=menu, name="menu"),
+    path('home/', views.home, name="home"),
+    path('about/', views.about, name="about"),
+    path('menu/', views.menu, name="menu"),
+    path('book/', views.book, name="book"),
 ]
--- Django/playground/1_environment/myproject/myapp/views.py
+++ Django/playground/1_environment/myproject/myapp/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, "home.html")
+    return render(request, "index.html")
 
 def menu(request):
-    return render(request, "menu")
+    return render(request, "menu.html")
 
 def about(request):
-    return render(request, "request")
+    return render(request, "about.html")
 
 def book(request):
-    return render(request, "book")
+    return render(request, "book.html")
