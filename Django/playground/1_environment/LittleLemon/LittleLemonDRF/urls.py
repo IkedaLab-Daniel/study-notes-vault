0085059aad279f818fb4acdbef1282fc49501f60
Ikedalab-Daniel <callejasmark63@gmail.com>
2026-03-16 03:13:19 +0800

    DRF: Lab 3 - Done

--- Django/playground/1_environment/LittleLemon/LittleLemonDRF/urls.py
+++ Django/playground/1_environment/LittleLemon/LittleLemonDRF/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from .views import SingleMenuItemView, MenuItemView
 
 urlpatterns = [
-
+    path('menu/', MenuItemView.as_view()),
+    path('menu/<int:pk>', SingleMenuItemView.as_view())
 ]
