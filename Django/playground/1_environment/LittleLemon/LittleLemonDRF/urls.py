51482a29a078e274948ddf9f79d2631616946aa4
Ikedalab-Daniel <callejasmark63@gmail.com>
2026-03-17 10:00:00 +0800

    Django: DRF Lab 4 - views.py - CategoriesView, MenuItemView

--- Django/playground/1_environment/LittleLemon/LittleLemonDRF/urls.py
+++ Django/playground/1_environment/LittleLemon/LittleLemonDRF/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-from .views import SingleMenuItemView, MenuItemView
 
 urlpatterns = [
-    path('menu/', MenuItemView.as_view()),
-    path('menu/<int:pk>', SingleMenuItemView.as_view())
+    # path('menu/', MenuItemView.as_view()),
+    # path('menu/<int:pk>', SingleMenuItemView.as_view())
 ]
--- Django/playground/1_environment/LittleLemon/LittleLemonDRF/views.py
+++ Django/playground/1_environment/LittleLemon/LittleLemonDRF/views.py
@@ -1,11 +1,14 @@
-from .models import MenuItem
-from .serializers import MenuItemSerializer
 from rest_framework import generics
+from .models import MenuItem, Category
+from .serializers import MenuItemSerializer, CategorySerializer
 
-class MenuItemView(generics.ListCreateAPIView):
+class CategoriesView(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+class MenuItemsView(generics.ListCreateAPIView):
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
-
-class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = MenuItem.objects.all()
-    serializer_class = MenuItemSerializer
+    ordering_fields = ['price', 'inventory']
+    search_fields = 'category'
+    
