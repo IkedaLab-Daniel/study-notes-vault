f63edddb8ed2980359690f771dd3a181e296dd6e
Ikedalab-Daniel <callejasmark63@gmail.com>
2026-04-16 10:00:00 +0800

    Next JS - Intro to JS - Pages

---
+++ Next/iceice/app/about/page.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function About() {
+    return (
+        <>
+            <h1 className="text-6xl font-bold">About Me</h1>
+            <Link href="/">Back</Link>
+        </>
+    )
+}
--- Next/iceice/app/page.js
+++ Next/iceice/app/page.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
         height={200}
         priority
       />
+      <Link href="/about">Go to About Page</Link>
     </main>
   );
 }
