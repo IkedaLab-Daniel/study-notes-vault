27f93971b789ad3903229387fc58ecd4abe3b3c9
Ikedalab-Daniel <callejasmark63@gmail.com>
2026-04-17 23:17:18 +0800

    Next - Intro to Next: Styling in Next.js Demo

--- Next/iceice/app/posts/[id]/page.js
+++ Next/iceice/app/posts/[id]/page.js
@@ -1,5 +1,8 @@
-import Image from "next/image"
-import { posts } from "../../data/posts"
+import Image from "next/image";
+import { posts } from "../../data/posts";
+import styles from "./Post.module.css";
+import Link from "next/link";
+import { ArrowLeft } from "lucide-react";
 
 export default async function Post({ params }) {
     const { id } = await params
@@ -12,17 +15,21 @@ export default async function Post({ params }) {
     }
 
     return (
-        <main className="flex flex-col justify-center items-center gap-2">
-            <h1 className="text-4xl">Post ID: {post.id}</h1>
-            <h1 className="text-4xl">Post Title: {post.title}</h1>
-            <h1 className="text-4xl">Post Content: {post.content}</h1>
+        <main className={styles.container}>
+            <h1 className={styles.title}>{post.title}</h1>
+            <h1 className={styles.content}>{post.content}</h1>
             <Image 
                     src="https://media.tenor.com/v7lgy2BsGR8AAAAe/shocked-black-guy-shocked.png"
                     alt="wow"
                     width={200}
                     height={200}
                     priority
+                    className="mb-4"
                   />
+            <Link href="/" className="text-sm text-slate-300 flex items-center gap-1 hover:scale-110 hover:text-white hover:font-bold transition duration-500">
+                <ArrowLeft size={16} />
+                <span>Back</span>
+            </Link>
         </main>
-    )
+    );
 }
