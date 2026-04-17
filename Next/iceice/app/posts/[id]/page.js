91cb5a6d49de07d2ce00177bd7bfe15acc008042
Ikedalab-Daniel <callejasmark63@gmail.com>
2026-04-17 22:49:31 +0800

    Next - Intro to Next: Data Fetching with Next JS Demo

--- Next/iceice/app/posts/[id]/page.js
+++ Next/iceice/app/posts/[id]/page.js
@@ -1,11 +1,21 @@
 import Image from "next/image"
+import { posts } from "../../data/posts"
 
 export default async function Post({ params }) {
     const { id } = await params
+    const post = posts.find((post) => post.id === id);
+
+    if (!post) {
+        return (
+            <h1>! Post not found !</h1>
+        )
+    }
 
     return (
         <main className="flex flex-col justify-center items-center gap-2">
-            <h1 className="text-4xl">Post ID: {id}</h1>
+            <h1 className="text-4xl">Post ID: {post.id}</h1>
+            <h1 className="text-4xl">Post Title: {post.title}</h1>
+            <h1 className="text-4xl">Post Content: {post.content}</h1>
             <Image 
                     src="https://media.tenor.com/v7lgy2BsGR8AAAAe/shocked-black-guy-shocked.png"
                     alt="wow"
