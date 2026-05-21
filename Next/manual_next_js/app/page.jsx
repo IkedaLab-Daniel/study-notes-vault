7b91aa6fc6d0d85d04dc36e911d7b6fa76cfece7
Ikedalab-Daniel <callejasmark63@gmail.com>
2026-05-22 02:48:21 +0800

    create product page

--- Next/manual_next_js/app/page.jsx
+++ Next/manual_next_js/app/page.jsx
@@ -3,6 +3,9 @@ export default function Page() {
         <>
             <h1>Hello, Next.js!!!!</h1>
             <a href="/about" style={{color: 'white'}}>About</a>
+            
+            <br /><br/>
+            <a href="/products" style={{color: 'white'}}>Products</a>
         </>
     )
 }
---
+++ Next/manual_next_js/app/products/page.jsx
@@ -0,0 +1,8 @@
+export default function ICEICE() {
+    return (
+        <>
+            <h1>This is the product page!!!</h1>
+            <a href="/" style={{color: 'white'}}>Home</a>
+        </>
+    )
+}
